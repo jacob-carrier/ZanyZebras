34085a249c95b5f3b29afe39a076aa53cf0c5477
Jacob Carrier <jacob.carrier@gmail.com>
2012-03-08 13:46:58 -0500

    Added new files GameInput, GamePlayScreen and a Utils file.

---
+++ Zany Zebras/GameInput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace Zany_Zebras
+{
+    class GameInput : Input
+    {
+        private Level levelRef;
+        private GamePlayScreen gpScreen;
+        public GameInput(GamePlayScreen screen, Level level)
+        {
+            levelRef = level;
+            gpScreen = screen;
+        }
+        
+        public override void Update()
+        {
+            newState = Mouse.GetState();
+
+            if (mouseDown())
+            {
+                gpScreen.ShowMouseText = true;
+            }
+
+            if (mouseUp())
+            {
+                gpScreen.RenderTileOutline = true;
+            }
+
+            oldState = newState;
+        }
+    }
+}
---
+++ Zany Zebras/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zany_Zebras
+{
+    class Utils
+    {
+        private static Utils _instance;
+        public static Utils Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Utils();
+                }
+
+                return _instance;
+            }
+        }
+
+        public Utils()
+        {
+
+        }
+
+        public void drawTileOutline(Tile t)
+        {
+            Game1.Instance.SpriteBatch.Draw(Game1.Instance.gameContent.Load<Texture2D>("Sprites/outline"), t.BoundingBox, Color.White);
+        }
+
+        public bool BoundingBoxCollision(Rectangle r1, Rectangle r2)
+        {
+            return r1.Intersects(r2);
+        }
+    }
+}
